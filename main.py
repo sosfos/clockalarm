aec1f8d9d1344599475a4c2174fea22f607f5d96
sosfos <sosfos@126.com>
2016-03-28 22:37:29 +0800

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+import kivy
+from random import Random
+kivy.require('1.9.0')
+
+from kivy.app import App
+
+class ClockApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    app = ClockApp()
+    app.run()
